268ce8de8dbc524579ca9cac49e73d0f6b9ad9d5
Molly Wohl <mollywohl@OSX-ToppLab11.local>
2018-07-26 19:15:50 -0700

    Get and print output

--- python-join.py
+++ python-join.py
@@ -52,12 +52,48 @@ def get_joint_keys(comparison_key_list, infile, key_index):
         joint_keys.append(line_list[key_index])
   return joint_keys
 
+def get_left_and_joint_output(left_file, right_file, left_index, right_index, joint_keys):
+  with open(left_file) as lf:
+    for line in lf:
+      clean_line = line.rstrip('\n')
+      line_list = clean_line.split(" ")
+      key = line_list.pop(left_index)
+      out = []
+      out.append(key)
+      for item in line_list:
+        out.append(item)
+      if key in joint_keys:
+        with open(right_file) as rf:
+          for right_line in rf:
+            right_clean_line = right_line.rstrip('\n')
+            right_line_list = right_clean_line.split(" ")
+            if right_line_list[right_index] == key:
+              right_line_list.pop(right_index)
+              for right_item in right_line_list:
+                out.append(right_item)
+      print(out)
+
+def get_right_output(right_file, right_index, joint_keys): 
+  with open(right_file) as f:
+    for line in f:
+      clean_line = line.rstrip('\n')
+      line_list = clean_line.split(" ")
+      key = line_list.pop(right_index)
+      if key not in joint_keys:
+        out = []
+        out.append(key)
+        for item in line_list:
+          out.append(item)
+        print(out)
+
+
 def main(): 
   left_file, right_file, left_index, right_index = get_arguments()
   left_keys = get_keys(left_file, left_index)
-  print(left_keys)
   joint_keys = get_joint_keys(left_keys, right_file, right_index)
-  print(joint_keys)
+
+  get_left_and_joint_output(left_file, right_file, left_index, right_index, joint_keys)
+  get_right_output(right_file, right_index, joint_keys)
 
 if __name__ == "__main__":
   main()
